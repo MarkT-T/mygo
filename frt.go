c3d1a708277e8ee4bd6cd6fba13b102a267bccd7
MarkT-T <125831637+MarkT-T@users.noreply.github.com>
2023-03-04 21:29:52 +0200

    Add files via upload

---
+++ frt.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	frtdate := time.Date(1789, 6, 14, 0, 0, 0, 0, time.UTC)
+	now := time.Now()
+	diff := now.Sub(frtdate)
+
+	fmt.Println("The Great French Revolution began", int64(diff.Hours()/24/365), "years,", int64(diff.Hours()/24/30), "month and", int64(diff.Hours()/24), "days ago")
+}
