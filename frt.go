b23246f9b120fdc2ce4c2d6d269f2a1ce56302ce
MarkT-T <125831637+MarkT-T@users.noreply.github.com>
2023-03-04 23:19:34 +0200

    Update frt.go

--- frt.go
+++ frt.go
@@ -10,5 +10,5 @@ func main() {
 	now := time.Now()
 	diff := now.Sub(frtdate)
 
-	fmt.Println("The Great French Revolution began", int64(diff.Hours()/24/365), "years,", int64(diff.Hours()/24/30), "month and", int64(diff.Hours()/24), "days ago")
+	fmt.Println("The Great French Revolution began", int64(diff.Hours()/24/365), "years,", int64(diff.Hours()/24/30), "month or", int64(diff.Hours()/24), "days ago")
 }
